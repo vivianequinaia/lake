7bd578ec2213549319458ba822b2e47a2612311d
quinaiaviviane <viviane.quinaia@arquivei.com.br>
2020-04-29 09:46:45 -0300

    change YellowDuckRepository

--- app/Repositories/YellowDuckRepository.php
+++ app/Repositories/YellowDuckRepository.php
@@ -26,7 +26,6 @@ class YellowDuckRepository implements CountDucksGateway
                     $count++;
                 }
             }
-
             return new Duck($count);
         } catch (\Exception $exception) {
             throw new CountDucksDatabaseException();
